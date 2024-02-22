70e018efad57c92dc011caa0ec1fed6344df2ead
evil666in <158379561+evil666in@users.noreply.github.com>
2024-02-22 01:44:26 -0600

    Add files via upload

---
+++ EVA1_1_CLASE/src/eva1_1_clase/EVA1_1_CLASE.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package eva1_1_clase;
+
+/**
+ *
+ * @author evely
+ */
+public class EVA1_1_CLASE {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        /*CLASE PERSONA
+/*
+nombre de la clase --> nomenglatura CamelCase
+class Nombredelaclase {cuerpo de la clase}    
+*/
+
+class persona{
+    
+}
+    }
+    
+}
